f9bb1b090ee1e4467a40b977aa3f22c2cdb1195f
vortex1573 <155903047+vortex1573@users.noreply.github.com>
2024-01-10 10:11:40 -0600

    Disabled CreateMove hook.

--- CreateMove.cpp
+++ CreateMove.cpp
@@ -1,7 +1,7 @@
 #include "CreateMove.hpp"
 
 void CreateMove::Hook() {
-	hookObject->Create("CCSGOInput", "CreateMove", Memory::GetVirtual<void*>(nullptr, 14U), &Detour);
+	//hookObject->Create("CCSGOInput", "CreateMove", Memory::GetVirtual<void*>(nullptr, 14U), &Detour);
 }
 
 bool __fastcall CreateMove::Detour(void* input, unsigned int nSlot, int64_t unk1, int8_t unk2) {
